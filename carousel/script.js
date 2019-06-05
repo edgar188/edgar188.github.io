ea0e18c994ee45673dc713045880bd0be479e586
Edgar Hovhannisyan <47250748+edgar188@users.noreply.github.com>
2019-06-05 19:21:52 +0400

    Add files via upload

--- carousel/script.js
+++ carousel/script.js
@@ -6,29 +6,41 @@ const passHandler = side => {
 		img = document.querySelector("img"),
 		imgSource = img["src"].slice(img["src"].indexOf("/img"));
 
+	let arr = [
+	"/img/1.jpg",
+	"/img/2.jpg",
+	"/img/3.jpg",
+	"/img/4.jpg",
+	"/img/5.jpg",
+	"/img/6.jpg",
+	"/img/7.jpg",
+	"/img/8.jpg",
+	"/img/9.png",
+	"/img/10.png"
+	];
+
 	if (side === "left") {	
-		if (imgSource[5] === "1") {
-			img.src = "img/8.jpg";
+		if (imgSource === arr[0]) {
+			img.src = img["src"].replace(imgSource, arr[arr.length-1]);
 		} else {
-			let temp = imgSource.replace(imgSource[5], +imgSource[5]-1);
-			img.src = img["src"].replace(imgSource, temp);
+			let t = arr.indexOf(imgSource);
+			img.src = img["src"].replace(imgSource, arr[t-1]);
 		}
 	}
 	else {
-		if (imgSource[5] === "8") {
-			img.src = "img/1.jpg";
+		if (imgSource === arr[arr.length-1]) {
+			img.src = img["src"].replace(imgSource, arr[0]);
 		} else {
-			let temp = imgSource.replace(imgSource[5], +imgSource[5]+1);
-			img.src = img["src"].replace(imgSource, temp);
+			let t = arr.indexOf(imgSource);
+			img.src = img["src"].replace(imgSource, arr[t+1]);
 		}
 	}
 };
 
 let timer;
-
 const getSlideShow = () => {
-	const button = document.getElementById("button");
-	const buttonSpan = document.getElementById("buttonSpan");
+	const button = document.getElementById("button"),
+	buttonSpan = document.getElementById("buttonSpan");
 	
 	if (buttonSpan.children[0].innerText === "Play") {
 		buttonSpan.children[0].innerText = "Stop";
@@ -61,4 +73,8 @@ document.body.onkeypress = e => {
 document.querySelector("img").onclick = () => {
 	return getSlideShow();
 }
+
+
+
+
 
