4bd6ea01670ca553c01f653546a1810efa68286c
Edgar Hovhannisyan <47250748+edgar188@users.noreply.github.com>
2019-06-06 13:25:00 +0400

    Update script.js

--- carousel/script.js
+++ carousel/script.js
@@ -44,7 +44,7 @@ const getSlideShow = () => {
 	
 	if (buttonSpan.children[0].innerText === "Play") {
 		buttonSpan.children[0].innerText = "Stop";
-		return timer = setInterval(passHandler, 500);
+		return timer = setInterval(passHandler, 1000);
 	}
 
 	buttonSpan.children[0].innerText = "Play";
